3812ad185f1c9cd2ef4a57dc9a7d6af4b964bd04
kimhyoju123 <qjqmf217@naver.com>
2021-12-12 23:34:56 -0800

    lab2

---
+++ lab2/makeop/lib/multiply.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int multiply(int a, int b){
+  int result = a*b;
+  return result;
+}
---
+++ lab2/makeop/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "testlib.h"
+
+int main() 
+{
+	printf("ADD : %d \n", add(3,1));
+	printf("SUBTRACT : %d \n",subtract(3,1));
+	printf("MULTIPLY : %d \n",multiply(3,1));
+	printf("DIVIDE : %d \n", divide(3,1));
+}
